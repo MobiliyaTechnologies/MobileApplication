5f90a3e908aaae087480d6fccfcba0dc1729ed03
gaurav <gswable@gmail.com>
2017-02-09 19:54:54 +0530

    #14 user alert list and alert acknowledgement implemented

---
+++ CSU_PORTABLE/CSU_PORTABLE/CSU_PORTABLE.Droid/UI/AlertListAdapter.cs
@@ -0,0 +1,156 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.Widget;
+using CSU_PORTABLE.Models;
+using static Android.Views.View;
+using RestSharp;
+using Android.Util;
+using CSU_PORTABLE.Utils;
+using CSU_PORTABLE.Droid.Utils;
+using Newtonsoft.Json;
+using Android.Content.Res;
+using Android.Graphics;
+
+namespace CSU_PORTABLE.Droid.UI
+{
+    class AlertListAdapter : RecyclerView.Adapter
+    {
+        const string TAG = "AlertListAdapter";
+        public List<AlertModel> mAlertModels;
+
+        public AlertListAdapter(List<AlertModel> alertModels)
+        {
+            mAlertModels = alertModels;
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View itemView = LayoutInflater.From(parent.Context).
+                        Inflate(Resource.Layout.alert_item, parent, false);
+            AlertViewHolder vh = new AlertViewHolder(itemView);
+            return vh;
+        }
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            AlertViewHolder vh = holder as AlertViewHolder;
+            vh.textViewAlert.Text = mAlertModels[position].Alert_Desc;
+            vh.textViewClass.Text = mAlertModels[position].Class_Desc;
+            vh.textViewTime.Text = mAlertModels[position].Timestamp;
+            vh.alertId = mAlertModels[position].Alert_Id;
+            if(mAlertModels[position].Is_Acknowledged)
+            {
+                vh.textViewAck.Visibility = ViewStates.Gone;
+                vh.divider.Visibility = ViewStates.Gone;
+            } else
+            {
+                vh.textViewAck.Visibility = ViewStates.Visible;
+                vh.textViewAck.Click += delegate {
+
+                    var preferenceHandler = new PreferenceHandler();
+                    UserDetails userDetails = preferenceHandler.GetUserDetails();
+                    int userId = userDetails.User_Id;
+
+                    AlertAcknowledgeModel ackModel = new AlertAcknowledgeModel();
+                    ackModel.Alert_Id = vh.alertId;
+                    ackModel.Acknowledged_By = userDetails.First_Name + " " + userDetails.Last_Name;
+
+                    vh.textViewAck.SaveEnabled = false;
+                    vh.textViewAck.SetTextColor(Color.LightGray);
+                    mAlertModels[position].Is_Acknowledged = true;
+
+                    acknowledgeAlert(ackModel, userId);
+                };
+            }
+        }
+
+        public override int ItemCount
+        {
+            get { return mAlertModels.Count(); }
+        }
+
+        //view holder class
+        public class AlertViewHolder : RecyclerView.ViewHolder
+        {
+            public TextView textViewAlert { get; set; }
+            public TextView textViewClass { get; set; }
+            public TextView textViewTime { get; set; }
+            public TextView textViewAck { get; set; }
+            public View divider { get; set; }
+
+            public int alertId;
+
+            public AlertViewHolder(View itemView) : base (itemView)
+            {
+                textViewAlert = itemView.FindViewById<TextView>(Resource.Id.textViewAlert);
+                textViewClass = itemView.FindViewById<TextView>(Resource.Id.textViewClass);
+                textViewTime = itemView.FindViewById<TextView>(Resource.Id.textViewTime);
+                textViewAck = itemView.FindViewById<TextView>(Resource.Id.textViewAcknowledge);
+                divider = itemView.FindViewById(Resource.Id.divider);
+            }
+        }
+
+        private void acknowledgeAlert(AlertAcknowledgeModel acknowledgeModel, int userId)
+        {
+            if (userId != -1)
+            {
+                RestClient client = new RestClient(Constants.SERVER_BASE_URL);
+                Log.Debug(TAG, "getAlertList()");
+
+                var request = new RestRequest(Constants.API_ACKNOWLWDGE_ALERTS + "/" + userId, Method.POST);
+                request.RequestFormat = DataFormat.Json;
+                request.AddBody(acknowledgeModel);
+
+                client.ExecuteAsync(request, response =>
+                {
+                    Console.WriteLine(response);
+                    if (response.StatusCode != 0)
+                    {
+                        Log.Debug(TAG, "async Response : " + response.ToString());
+                        //RunOnUiThread(() => {
+                        acknowledgeAlertResponse((RestResponse)response);
+                        //});
+                    }
+                });
+            }
+            else
+            {
+                Log.Debug(TAG, "Invalid User Id. Please Login Again !");
+            }
+        }
+
+        private void acknowledgeAlertResponse(RestResponse restResponse)
+        {
+            if (restResponse != null && restResponse.StatusCode == System.Net.HttpStatusCode.OK && restResponse.Content != null)
+            {
+                Log.Debug(TAG, restResponse.Content.ToString());
+
+                AlertAcknowledgementResponseModel response = JsonConvert.DeserializeObject<AlertAcknowledgementResponseModel>(restResponse.Content);
+
+                if (response.Status_Code == Constants.STATUS_CODE_SUCCESS)
+                {
+                    Log.Debug(TAG, "acknowledgement Successful");
+                    //update list
+
+                }
+                else
+                {
+                    Log.Debug(TAG, "Acknowledgement Failed");
+                }
+            }
+            else
+            {
+                Log.Debug(TAG, "acknowledgeAlertResponse() Failed");
+            }
+        }
+    }
+}
